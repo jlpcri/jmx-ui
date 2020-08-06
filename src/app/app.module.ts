4fa8b966f8ea3a2abc779f60de088c887fd759e0
Songqing Liu <songqing.liu@amvholdings.com>
2020-08-06 00:13:22 -0500

    Add header and guide components to project GURU-1081

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import {FormsModule} from "@angular/forms";
+import { HeaderComponent } from './header/header.component';
+import { GuideComponent } from './guide/guide.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    HeaderComponent,
+    GuideComponent
   ],
   imports: [
     BrowserModule,
