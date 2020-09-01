494baa3d38c75c3d63685089d94b76c2f6b41590
Songqing Liu <songqing.liu@amvholdings.com>
2020-09-01 08:32:24 -0500

    Add method getRecipes etc.

--- src/main/java/com/amvholdings/jmxui/component/recipe/RecipeController.java
+++ src/main/java/com/amvholdings/jmxui/component/recipe/RecipeController.java
@@ -1,19 +1,22 @@
 package com.amvholdings.jmxui.component.recipe;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class RecipeController {
   private final RecipeDomain recipeDomain;
 
   @GetMapping("/jmx-ui/api")
   public List<RecipeModel> getRecipes(){
+    log.info("abcdefg");
     return recipeDomain.getRecipes();
   }
 
