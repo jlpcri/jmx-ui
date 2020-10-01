d5e2e98eb9273d56bbf67b131f30f2ee0329a93c
Joe Fago <joe.fago@amvholdings.com>
2020-10-01 06:37:48 -0500

    Tests are working - but no coverage yet. GURU-1354

--- src/app/recipe-list/recipe-list.component.spec.ts
+++ src/app/recipe-list/recipe-list.component.spec.ts
@@ -1,14 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecipeListComponent } from './recipe-list.component';
+import {RecipeListService} from './shared/recipe-list.service';
+import {IndexedDatabaseService} from '../shared/indexed-database.service';
 
 describe('RecipeListComponent', () => {
   let component: RecipeListComponent;
   let fixture: ComponentFixture<RecipeListComponent>;
 
   beforeEach(async(() => {
+    const recipeListServiceSpy = jasmine.createSpyObj('RecipeListService', ['retrieveAll']);
+    const idbServiceSpy = jasmine.createSpyObj('IndexedDatabaseService', ['getRecipesFromIdb']);
     TestBed.configureTestingModule({
-      declarations: [ RecipeListComponent ]
+      declarations: [ RecipeListComponent ],
+      providers: [
+        { provide: RecipeListService, useValue: recipeListServiceSpy},
+        { provide: IndexedDatabaseService, useValue: idbServiceSpy},
+      ]
     })
     .compileComponents();
   }));
