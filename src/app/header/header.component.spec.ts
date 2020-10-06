17c340cad315054485370ca0c93502c50d3499cc
Songqing Liu <songqing.liu@amvholdings.com>
2020-10-06 10:00:10 -0500

    Resolve header.component.spec.ts error

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -4,10 +4,14 @@ import { HeaderComponent } from './header.component';
 import {AuthService} from '../auth.service';
 import {of} from 'rxjs';
 import {User} from '../shared/user.model';
+import {HttpClient} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     const authServiceSpy = jasmine.createSpyObj('AuthService', ['authorized']);
@@ -22,6 +26,11 @@ describe('HeaderComponent', () => {
   }));
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
