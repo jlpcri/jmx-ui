06f6005d2c200aef0633e46edb7f3d540ebec747
Songqing Liu <songqing.liu@amvholdings.com>
2020-10-06 14:44:43 -0500

    Add admin check etc.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../shared/user.model';
-import { AuthService} from '../auth.service';
-import {IndexedDatabaseService} from '../shared/indexed-database.service';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../shared/user.model';
+import {AuthService} from '../auth.service';
 import {RecipeListService} from '../recipe-list/shared/recipe-list.service';
 
 @Component({
@@ -12,8 +11,7 @@ import {RecipeListService} from '../recipe-list/shared/recipe-list.service';
 export class HeaderComponent implements OnInit {
   user: User;
   constructor(private auth: AuthService,
-              private recipeListService: RecipeListService,
-              private idbService: IndexedDatabaseService) { }
+              private recipeListService: RecipeListService) { }
 
   ngOnInit(): void {
     this.auth.authorized().subscribe(
@@ -30,10 +28,16 @@ export class HeaderComponent implements OnInit {
   }
 
   emptyIdbData() {
-    this.idbService.clearData();
+    this.recipeListService.emptyIdbData();
   }
 
-  isDeveloper(): boolean {
-    return (typeof this.user !== 'undefined') && this.user.name === 'Songqing Liu';
+  isAdmin(): boolean {
+    if (this.user === undefined) {
+      return false;
+    } else if (this.user.roles === undefined || this.user.roles.length === 0) {
+      return false;
+    } else {
+      return this.user.roles.indexOf('GROUP - Alohma Admin') > 0;
+    }
   }
 }
