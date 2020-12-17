5dac27a40013169d36e4d128b1f1a7b2cebce5b4
Songqing Liu <songqing.liu@amvholdings.com>
2020-12-17 10:13:02 -0600

    Re-organize codes for bottleScan etc.  GURU-1727

--- src/app/recipe-list/recipe-list.component.spec.ts
+++ src/app/recipe-list/recipe-list.component.spec.ts
@@ -3,16 +3,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RecipeListComponent } from './recipe-list.component';
 import {RecipeListService} from './shared/recipe-list.service';
 import {IndexedDatabaseService} from '../shared/indexed-database.service';
+import {HttpClient} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('RecipeListComponent', () => {
   let component: RecipeListComponent;
   let fixture: ComponentFixture<RecipeListComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     const recipeListServiceSpy = jasmine.createSpyObj('RecipeListService', ['retrieveAll', 'retrieveLocations']);
     const idbServiceSpy = jasmine.createSpyObj('IndexedDatabaseService', ['getRecipesFromIdb', 'init']);
     TestBed.configureTestingModule({
       declarations: [ RecipeListComponent ],
+      imports: [HttpClientTestingModule],
       providers: [
         { provide: RecipeListService, useValue: recipeListServiceSpy},
         { provide: IndexedDatabaseService, useValue: idbServiceSpy},
@@ -22,6 +27,8 @@ describe('RecipeListComponent', () => {
   }));
 
   beforeEach(() => {
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     fixture = TestBed.createComponent(RecipeListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
