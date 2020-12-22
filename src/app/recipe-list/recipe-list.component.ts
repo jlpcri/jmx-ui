70fd670a590db16ec516f3c8d325ee11d1b04e94
Songqing Liu <songqing.liu@amvholdings.com>
2020-12-21 22:10:17 -0600

    Update initialize idb objectStore etc. GURU-1727

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -298,6 +298,7 @@ export class RecipeListComponent implements OnInit, OnDestroy {
         this.printData.storeLocation = location.storeLocation;
       },
       () => {
+        window.location.reload();
       }
     );
   }
